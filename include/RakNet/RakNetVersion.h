591c0326cf94a1caf705eb5e6b8b24e35184eced
vp817 <118466306+vp817@users.noreply.github.com>
2024-03-01 22:47:31 +0300

    Update RakNetVersion.h

--- include/RakNet/RakNetVersion.h
+++ include/RakNet/RakNetVersion.h
@@ -8,12 +8,10 @@
  *
  */
 
-#define RAKNET_VERSION "4.081"
+#define RAKNET_VERSION "5.00"
 #define RAKNET_VERSION_NUMBER 5.00
 #define RAKNET_VERSION_NUMBER_INT 500
 
-#define RAKNET_DATE "11/11/2023"
+#define RAKNET_DATE "1/3/2024"
 
-// What compatible protocol version RakNet is using. When this value changes, it indicates this version of RakNet cannot connection to an older version.
-// ID_INCOMPATIBLE_PROTOCOL_VERSION will be returned on connection attempt in this case
-#define RAKNET_PROTOCOL_VERSION 6
+#define RAKNET_PROTOCOL_VERSION 7
